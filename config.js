32600680206bf6179dcd7de328373f085a5f8f6f
huuhieucsb15 <139659826+huuhieucsb15@users.noreply.github.com>
2023-07-16 08:25:04 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Bạn Yêu À !',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'Đồng Ý',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn Cứ Là Thích Anh <33',
+    btnNo: 'Không, Anh Trai à :3',
+    question:'Trên thế giới gần 8 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333 , nhưng vẫn mãi Yêu Phúc ♥️',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Nhắn Tin Cho Anh Nhé Công Chúa ♥️.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://m.me/azz.phuc' 
+}
